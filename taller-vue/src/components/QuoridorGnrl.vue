b38584edc58989006ba79dced60b10fcb2449012
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-05-26 23:21:52 -0500

    Carpetas
    Se borraron carpetas que sobran y se añadieron otras nuevas para implementación de atomic design

---
+++ taller-vue/src/components/QuoridorGnrl.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pagina-juego">
+    <GameTemplate/>
+  </div>
+</template>
+<script>
+import GameTemplate from '../components/plantillas/GameTemplate.vue';
+
+export default {
+  components: {GameTemplate},
+};
+</script>
+
+<style scoped>
+.game-page{
+  padding: 20px;
+}
+</style>
