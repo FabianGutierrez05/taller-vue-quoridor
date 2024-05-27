593808d4e7ccc03589717f6f6047fc10f13f7963
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-05-27 00:57:06 -0500

    Actualizados componentes de moleculas
    se añadio el contenido de los componentes en la carpeta de moleculas para los controles de movimiento y el tablero de juego

---
+++ taller-vue/src/components/moleculas/ControlesMov.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+    props:{
+        MurosActuales: Number,
+        ModoAccion: String
+    }
+};
+</script>
+
+<template>
+    <div class="ControlMov">
+        <button @click="$emit('ActivModoAccion')">{{ ModoAccion === 'mover' ? 'Cambiar a Poner  Muro' : 'Cambiar a Moverse'}}</button>
+        <div v-if="ModoAccion === 'muro'">
+            <p>Selecciona la orientacion del muro:</p>
+            <button @click="$emit('setOrientacionMuro', 'horizontal')">Horizontal</button>
+            <button @click="$emit('setOrientacionMuro', 'vertical')">Vertical</button>
+            <p>Muros restantes: {{ MurosActuales }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.ControlMov {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ taller-vue/src/components/moleculas/FilaTablero.vue
@@ -0,0 +1,46 @@
+<script>
+    import Celda from '../atomos/Celda.vue';
+
+    export default {
+        props: {
+            fila: Array,
+            jugadores: Array
+        },
+        components: {Celda},
+        methods: {
+            PosJ1(fila, columna) {
+                const J1 = this.jugadores[0];
+                return J1.fila === fila && J1.columna === columna;
+            },
+            PosJ2(fila, columna) {
+                const J2 = this.jugadores[1];
+                return J2.fila === fila && J2.columna === columna;
+            },
+            MnjClickCelda({fila, columna}) {
+                this.$emit('CeldaClickeada', {fila: this.fila, columna: this.columna})
+            },
+        },
+    };
+</script>
+
+<template>
+    <div class="filaTablero">
+        <Celda
+        v-for="celda in fila"
+        :key="celda.columna"
+        :fila="celda.fila"
+        :columna="celda.columna"
+        :Jugador1="Jugador1(celda.fila, celda.columna)"
+        :Jugador2="Jugador2(celda.fila, celda.columna)"
+        :MuroVert="celda.MuroVert"
+        :MuroHori="celda.MuroHori"
+        @cellClicked="handleCellClick"
+        />
+    </div>
+</template>
+
+<style scoped>
+.filaTablero {
+  display: flex;
+}
+</style>
