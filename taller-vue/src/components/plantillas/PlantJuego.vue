2160b77a3f8eeb2a4bbf68a1b2021f9199c20f29
FabianGutierrez05 <fcgutierrez@uniboyaca.edu.co>
2024-05-27 01:35:36 -0500

    Organismos y plantilla
    Se añadio la logica de los organismos y se inicializo la plantilla

---
+++ taller-vue/src/components/plantillas/PlantJuego.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="PlantJ" @keydown="TeclasFlechas" tabindex="0">
+        <MsgTurno :mensaje="msgTurno"/>
+        <TabJuego :tablero="tablero" :jugadores="jugadores"  @CeldaClickeada="MnjClickCelda" />
+        <ControlesMov :MurosActuales="MurosActuales" :ModoAccion="ModoAccion" @mover="MnjMov" @PonerMuro="MnjMuroPos" @reiniciar="reiniciarJuego" @cambiarModo="cambiarModoAccion" @MuroOrientacion="PonerOrientacion"/>
+
+    </div>
+</template>
+<script>
+import MsgTurno from '../organismos/MsgTurno.vue';
+import TabJuego from '../organismos/TabJuego.vue';
+import ControlesMov from '../moleculas/ControlesMov.vue';
+
+export default{
+    components: {MsgTurno, TabJuego, ControlesMov},
+    data()  {
+        return {
+            tablero: this.IniciarTablero(),
+            jugadores: [
+                {id:1, fila:0, columna:4, Muros:10, MetaFila:8},
+                {id:2, fila:8, columna:4, Muros:10, MetaFila:0}
+            ],
+            JugActual: 0,
+            Ganador: null,
+            msgTurno: 'Turno del jugador 1',
+            ModoAccion: 'mover',
+            OrientacionMuro: 'horizontal',
+        };
+    },
+    computed: {
+        
+    }
+}
+</script>
+<style>
+    .PlantJ{
+        display: flex;
+  flex-direction: column;
+  align-items: center;
+    }
+</style>
